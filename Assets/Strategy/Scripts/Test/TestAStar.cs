6a4f279e01c62e21b32c16e24c62ca7724062c46
infinityww88 <138868906+infinityww88@users.noreply.github.com>
2024-01-22 20:56:53 +0800

    A star test.

---
+++ Assets/Strategy/Scripts/Test/TestAStar.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+using Sirenix.OdinInspector;
+
+namespace Strategy {
+	
+	public class TestAStar : MonoBehaviour
+	{
+		public Transform targetPos;
+	
+		private Seeker seeker;
+		private RichAI richAI;
+		private NavmeshCut navmeshCut;
+	
+		public float reachRadius = 0.1f;
+	
+		// Start is called before the first frame update
+		void Start()
+		{
+			seeker = GetComponent<Seeker>();
+			richAI = GetComponent<RichAI>();
+			navmeshCut = GetComponent<NavmeshCut>();
+		}
+
+		[Button]
+		void StartPath() {
+			seeker.StartPath(transform.position, targetPos.position, OnPathComplete);
+		}
+	
+		void OnPathComplete(Path path) {
+		
+		}
+	
+		// Update is called every frame, if the MonoBehaviour is enabled.
+		protected void Update()
+		{
+			if ((transform.position - targetPos.position).XZ().magnitude < reachRadius) {
+				richAI.isStopped = true;
+				richAI.enabled = false;
+				seeker.enabled = false;
+				navmeshCut.enabled = true;
+			}
+		}
+	}
+}
+
